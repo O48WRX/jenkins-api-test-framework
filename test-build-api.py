001d21c418a468ea629b555b13b448fefffb6bf7
O48WRX <kardyy81@gmail.com>
2026-09-17 18:57:56 +0200

    Added building test python file

---
+++ test-build-api.py
@@ -0,0 +1,13 @@
+from os import system
+import time
+
+print('Building project')
+time.sleep(3)
+
+print('Building project.')
+time.sleep(3)
+print('Building project..')
+time.sleep(3)
+print('Building project...')
+time.sleep(3)
+print('Build Successful!')
