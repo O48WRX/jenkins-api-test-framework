c9b5f0de2c495197705f230c4c2a69589878c8c5
O48WRX <kardyy81@gmail.com>
2026-09-17 19:11:56 +0200

    Added disclaimer to test file

--- test-build-api.py
+++ test-build-api.py
@@ -1,6 +1,11 @@
 from os import system
 import time
 
+print('This file is just a test file and does not build anything')
+print('')
+print('This is just to similate how a real building file would work')
+print('')
+
 print('Building project')
 time.sleep(3)
 
